ec35c04c7c84b0172c7d2104006b972f57c3429b
CelerStar <celerstar@celerstar.com>
2017-07-11 00:09:17 +0800

    nbstrap v1.0

---
+++ js/nbstrap.js
@@ -0,0 +1,30 @@
+/*!
+ * NBstrap v1.0.0
+ * Copyright 2017-2018 NBuilder, Inc.
+ * Licensed under MIT
+ */
+var nbuilderWindowHeight = 0;
+var nbuilderTitlebarHeight = 0;
+var nbuilderContentHeight = 0;
+
+$(window).ready(function() {
+	nbuilderWindowHeight = $(window).height();
+	nbuilderTitlebarHeight = $(".nb-titlebar").height();
+	nbuilderContentHeight = nbuilderWindowHeight - nbuilderTitlebarHeight;
+	
+	nbuilder_resize();
+});
+
+$(window).resize(function() {
+	nbuilderWindowHeight = $(window).height();
+	nbuilderTitlebarHeight = $(".nb-titlebar").height();
+	nbuilderContentHeight = nbuilderWindowHeight - nbuilderTitlebarHeight;
+	
+	nbuilder_resize();
+});
+
+function nbuilder_resize() {
+	$(".nb-window").height(nbuilderWindowHeight);
+	$(".nb-content").height(nbuilderContentHeight);
+	
+}
