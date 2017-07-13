5b0be330cd8d8a8a150edfabad3b4f7c17d31943
CelerStar <celerstar@celerstar.com>
2017-07-14 01:37:37 +0800

    添加 NBstrap NBuilder UI 框架 设计方案类似于 bootstrap 包括
    垂直栅格组件:nb-window nb-container-fluid,nb-col,nb-row-xs-xx;
    标题内容组件:nb-titlebar,nb-titlebar-btn,nb-titlebar-element,nb-win-min,nb-win-max,nb-win-close;

--- js/nbstrap.js
+++ js/nbstrap.js
@@ -1,20 +1,42 @@
 /*!
- * NBstrap v1.0.0
+ * NBstrap v1.2.0
  * Copyright 2017-2018 NBuilder, Inc.
  * Licensed under MIT
  */
-var nbuilderWindowHeight = 0;
-var nbuilderTitlebarHeight = 0;
-var nbuilderContentHeight = 0;
+var nbuilderWindowHeight = 0; //窗体高度
+var nbuilderTitlebarHeight = 0; //标题高度
+var nbuilderContentHeight = 0; //内容高度
+var nbuilderWindowObject; //窗体对象
+var nbuilderWindowMaxState = false; //大小对象
 
+/*
+ * 名称：页面 ready 事件
+ * 输入：
+ * 输出：
+ * 描述：
+ */
 $(window).ready(function() {
 	nbuilderWindowHeight = $(window).height();
 	nbuilderTitlebarHeight = $(".nb-titlebar").height();
 	nbuilderContentHeight = nbuilderWindowHeight - nbuilderTitlebarHeight;
+	nbuilderWindowObject = nw.Window.get();
 
+	nbuilderWindowObject.on('maximize', function() {
+		nbuilderWindowMaxState = true;
+	});
+
+	nbuilderWindowObject.on('restore', function() {
+		nbuilderWindowMaxState = false;
+	});
 	nbuilder_resize();
 });
 
+/*
+ * 名称：页面 resize 事件
+ * 输入：
+ * 输出：
+ * 描述：
+ */
 $(window).resize(function() {
 	nbuilderWindowHeight = $(window).height();
 	nbuilderTitlebarHeight = $(".nb-titlebar").height();
@@ -23,12 +45,44 @@ $(window).resize(function() {
 	nbuilder_resize();
 });
 
+/*
+ * 名称：刷新 标题栏 内容高度
+ * 输入：
+ * 输出：
+ * 描述：
+ */
 function nbuilder_resize() {
-	$(".nb-window").height(nbuilderWindowHeight);
-	$(".nb-content").height(nbuilderContentHeight);
-
+	$(".nb-window").height(nbuilderWindowHeight); //设置最大框架高度
+	$(".nb-content").height(nbuilderContentHeight); //设置包含高度
 }
 
+/*
+ * 名称： 
+ * 输入：
+ * 输出：
+ * 描述：
+ */
 document.onselectstart = function() {
 	return false;
 }
+
+
+//关闭窗体事件
+$(".nb-win-close").click(function() {
+	nbuilderWindowObject.close();
+});
+
+//最大化窗体事件
+$(".nb-win-max").click(function() {
+	if(nbuilderWindowMaxState == false) {
+		nbuilderWindowObject.maximize();
+	} else {
+		nbuilderWindowObject.restore()
+	}
+});
+
+//最小化窗体事件
+$(".nb-win-min").click(function() {
+	nbuilderWindowObject.minimize();
+
+});
